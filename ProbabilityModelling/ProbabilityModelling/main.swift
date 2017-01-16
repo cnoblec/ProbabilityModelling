2af060138215dfa631e69a8e5cfbc9560b295fbb
rsgc-noblecurveira-c <student@macbook-pro-carlos-noble-curviera.local>
2017-01-16 14:31:50 -0500

    Initial Commit

---
+++ ProbabilityModelling/ProbabilityModelling/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ProbabilityModelling
+//
+//  Created by Carlos Noble Curveira on 2017-01-16.
+//  Copyright © 2017 Carlos Noble Curveira. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
